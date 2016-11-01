26c8802ed280fbee860b1b624ed4f980f561546e
Nick Chemsak <nchemsak@gmail.com>
2016-11-01 09:32:40 -0500

    add music link hidden

--- javascripts/add.js
+++ javascripts/add.js
@@ -2,11 +2,10 @@ var addLink = document.getElementById("link-add");
 var addView = document.getElementById("add-view");
 
 addLink.addEventListener("click", function() {
-  homeView.classList.add("hidden");
+  console.log("addLink: ", addLink);
+  // homeView.classList.add("hidden");
   listView.classList.add("hidden");
 
   addView.classList.add("visible");
   addView.classList.remove("hidden");
-
-
 });
--- javascripts/list.js
+++ javascripts/list.js
@@ -2,8 +2,9 @@ var listLink = document.getElementById("link-list");
 var listView = document.getElementById("list-view");
 
 listLink.addEventListener("click", function(event) {
+  console.log("listLink: ", listLink);
   event.preventDefault();
-  homeView.classList.add("hidden");
+  // homeView.classList.add("hidden");
   addView.classList.add("hidden");
 
   listView.classList.add("visible");
