f1d02739a92177be7ef106c90bd5c37c14ff3327
Nick Chemsak <nchemsak@gmail.com>
2016-11-03 15:11:05 -0500

    final version 3

--- javascripts/add.js
+++ javascripts/add.js
@@ -3,7 +3,7 @@ var addLink = document.getElementById("link-add");
 var addView = document.getElementById("add-view");
 
 addLink.addEventListener("click", function() {
-  console.log("addLink: ", addLink);
+  // console.log("addLink: ", addLink);
   // homeView.classList.add("hidden");
   listView.classList.add("hidden");
 
@@ -14,9 +14,6 @@ addLink.addEventListener("click", function() {
 
 function add() {
   document.getElementById("num1");
-  var add = (parseFloat(num1.value) + parseFloat(num2.value) );
+  var add = (parseFloat(num1.value) + parseFloat(num2.value));
   output.innerHTML = add;
 }
-
-
-
--- javascripts/songs.js
+++ javascripts/songs.js
@@ -1,31 +1,18 @@
 'use strict';
 var songs = [];
-var output = document.getElementById("songs");
-// var title = document.getElementsByClassName("songchoices");
+var output = document.getElementsByClassName("songs");
 
-console.log("output: ", output);
-songs[songs.length] = "Grand Canyon > by Puscifer on the album Money Shot"
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-songs[songs.length] = "Passenger > by Deftones on the album White Pony";
-
-
-songs.forEach(function(item) {
-  var newer = item.replace('>', '-');
-  console.log("newer: ", newer);
-  // output.innerHTML += "<li>" + newer + "</li>";
-  output.innerHTML += newer + "<br>";
-});
-
-
-// output.innerHTML = "<li class='test'>" + songs + "</li>";
-// output.innerHTML = "<div>" + songs + "</div>";
-
+// console.log("output: ", output);
+// songs[songs.length] = "Grand Canyon > by Puscifer on the album Money Shot"
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+// songs[songs.length] = "Passenger > by Deftones on the album White Pony";
 
 document.getElementById("addButton").addEventListener("click", addMusic);
+
 function addMusic() {
   var userInput = {
     title: document.getElementsByClassName("addForm")[0].value,
@@ -33,20 +20,39 @@ function addMusic() {
     album: document.getElementsByClassName("addForm")[2].value
   };
   songs.push(userInput);
-console.log("songs: ", songs);
-console.log("userInput.title: ", userInput.title);
-console.log("userInput.artist: ", userInput.artist);
-console.log("userInput.album: ", userInput.album);
-console.log("userInput: ", userInput);
+
+  var songTitle = document.createElement("h4");
+  songTitle.setAttribute("class", "songchoices");
+
+  var artistName = document.createElement("li");
+  artistName.setAttribute("class", "stuff");
+
+  var albumName = document.createElement("li");
+  albumName.setAttribute("class", "stuff");
+
+  console.log("songs: ", songs);
+  // console.log("songs[0].title: ", songs[0].title);
+
+  var title = document.createTextNode(userInput.title);
+  var artist = document.createTextNode(userInput.artist);
+  var album = document.createTextNode(userInput.album);
+
+  songTitle.appendChild(title);
+  artistName.appendChild(artist);
+  albumName.appendChild(album);
+
+  // document.body.appendChild(artistName);
+  document.getElementById("songs").appendChild(songTitle);
+  document.getElementById("songs").appendChild(artistName);
+  document.getElementById("songs").appendChild(albumName);
+
+  for (var property in userInput) {
+    // console.log(property + " = " + userInput[property]);
+    // console.log(property, userInput[property]);
+    output.innerHTML = userInput[property];
+  }
 }
 
-
-
-
-
-// output.innerHTML = "<div class='test'>" + userInput.title + "</div>";
-// output.innerHTML = "<div class='test'>" + songs.toString(addMusic) + "</div>";
-// output.innerHTML = userInput.artist;
-// output.innerHTML = userInput.album;
-
-// Once the user fills out the song form and clicks the add button, you should collect all values from the input fields, add the song to your array of songs, and update the song list in the DOM.
+var result = songs.map(function(song) {
+  return song
+});
